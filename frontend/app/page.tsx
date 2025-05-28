9cf082c2aaa9036ce05589a3ec20643a46a6a6cc
kibet nathan <nathanrufus540@gmail.com>
2025-05-28 16:40:58 +0300

    Doing frontend setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+ <div className="text-2xl font-bold text-blue-600">
+  Tailwind is working 🎉
+</div>
+
+  )
+ 
+
+};
+
+export default page;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#70B244",
+          dark: "#1B1D30",
+          light: "#FFFFFF",
+        },
+        fontFamily: {
+          sans: 'var(--font-sans), system-ui, sans-serif',
+          mono: 'var(--font-mono), Menlo, monospace',
+        },
+        colors: {
+          background: "var(--background)",
+          foreground: "var(--foreground)",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
