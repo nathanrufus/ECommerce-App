788a2e9c50ff9e7ffad9f7648526004667da2012
nathanrufus <nathanrufus540@gmail.com>
2025-06-06 15:00:54 +0300

    solved the redis error

--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -1,5 +1,11 @@
 const Redis = require('ioredis');
 
-const redis = new Redis(process.env.REDIS_URL); // Use full Upstash URL
+const redis = new Redis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: 1,
+  tls: {}  // This is what enables secure TLS
+});
+
+redis.on('connect', () => console.log('✅ Redis connected'));
+redis.on('error', (err) => console.error('❌ Redis error:', err));
 
 module.exports = redis;
